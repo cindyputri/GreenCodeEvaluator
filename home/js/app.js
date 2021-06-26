a9c94d5d7ca5b92f2382d038b5851886948d42ad
Amalia <amaliapdvh@gmail.com>
2021-06-26 08:11:32 +0200

    feat: home almost done

--- home/js/app.js
+++ home/js/app.js
@@ -1,1 +1,32 @@
 'use strict'
+let validated    = document.querySelectorAll('.validate')
+let uri          = document.querySelector('#validate-by-uri')
+let upload       = document.querySelector('#validate-by-upload')
+let directInput  = document.querySelector('#validate-by-input')
+
+validated.forEach((oneValidate, i) => {
+    oneValidate.addEventListener('click', () => {
+        oneValidate.classList.toggle('selected')        
+    })
+})
+
+function linkfunctionUri() {
+    window.location.href="#validate-by-uri";
+    uri.style.display = 'block'
+    upload.style.display = 'none'
+    directInput.style.display = 'none'
+
+}
+function linkfunctionUpload() {
+    window.location.href="#validate-by-upload";
+    uri.style.display = 'none'
+    upload.style.display = 'block'
+    directInput.style.display = 'none'
+
+}
+function linkfunctionInput() {
+    window.location.href="#validate-by-iput";
+    uri.style.display = 'none'
+    upload.style.display = 'none'
+    directInput.style.display = 'block'
+}
